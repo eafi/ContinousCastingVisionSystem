5a81ca77b5411a65dce258e4980b942c3a2e6c41
eafi <imeafi@gmail.com>
2022-03-22 20:47:51 +0800

    Added timeout

--- Modules/network.py
+++ Modules/network.py
@@ -9,7 +9,7 @@ import numpy as np
 from Modules.LOG import *
 import struct
 from collections import namedtuple
-PORT = 6669
+PORT = 6674
 import queue
 
 
@@ -21,6 +21,10 @@ class AbstractMsg(QObject):
         self.recCtlBits = np.uint32(0)
         self.recData = 9 * [np.float32(0.0)]
         self.format_ = "I9f"
+
+
+    def recv(self, bitData):
+        self.recCtlBits, self.recData = self.parse(bitData)
 
     def parse(self, data):
         if data and len(data) == 40:
@@ -37,7 +41,7 @@ class AbstractMsg(QObject):
             data = 9 * [np.float32(0.0)]
         if ctl is None:
             ctl = np.uint32(0)
-        if not data == 9 * [np.float32(0)] and not ctl == np.uint32(0):
+        if not data == 9 * [np.float32(0)] or not ctl == np.uint32(0):
             try:
                 DATA = namedtuple("DATA", "uCtl fData0 fData1 fData2 fData3 fData4 fData5 fData6 fData7 fData8")
                 msg_to_send = DATA(uCtl=ctl,
@@ -74,15 +78,15 @@ class AbstractMsg(QObject):
 
 class Network(QThread):
     robotCommunicationStatusSignal = pyqtSignal(str)
-    robotNewMsgignal = pyqtSignal(str)
     def __init__(self, ip='localhost', port=PORT):
         super(Network, self).__init__()
         self.ip = ip
         self.port = port
         # 解析报文内容
         self.msgManager = AbstractMsg()
+        # 用于保存PLC连接的套接子，所有相关指令将通过该文件发送。读取并不需要单独管理.
+        self.connectSocket = None
 
-    #TODO: 替换成select，当前网络代码没办法保证客户端断掉的检查
     def run(self):
         try:
             with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server:
@@ -90,11 +94,11 @@ class Network(QThread):
                 server.bind((self.ip, self.port))
                 server.listen(1)
                 inputs = [server]
-                outputs = []
-                message_queues = {}
+                self.outputs = []
+                self.message_queues = {}
                 while inputs:
                     print('wating for the next event')
-                    readable, writable, exceptional = select.select(inputs, outputs, inputs)
+                    readable, writable, exceptional = select.select(inputs, self.outputs, inputs, 5)
                     self.robotCommunicationStatusSignal.emit('OK')
                     for s in readable:
                         if s is server:
@@ -102,37 +106,37 @@ class Network(QThread):
                             print('connect from', client_address)
                             connection.setblocking(0)
                             inputs.append(connection)  # 同时监听这个新的套接子
+                            self.message_queues[connection] = queue.Queue()
+                            self.connectSocket = s
 
-                            message_queues[connection] = queue.Queue()
                         else:
                             # 其他可读客户端连接
                             data = s.recv(40)
                             if data:
                                 # 一个有数据的可读客户端
                                 print('  received {!r} from {}'.format(
-                                    data, s.getpeername()), file=sys.stderr,
-                                )
-                                self.msgManager.parse(data)
-                                self.robotNewMsgignal.emit(data)
-                                message_queues[s].put()
-                                ## 添加到输出列表用来做响应
-                                if s not in outputs:
-                                    outputs.append(s)
+                                    data, s.getpeername()), file=sys.stderr)
+                                #message_queues[s].put(data)
+                                # 解析新来的数据，并保存到msgManager中
+                                #self.msgManager.recv(data)
+                                self.connectSocket = s
+                                #if s not in outputs:
+                                #    outputs.append(s)
                             else: # 没有数据
                                 print('closing', client_address)
                                 self.robotCommunicationStatusSignal.emit('Break')
-                                if s in outputs:
-                                    outputs.remove(s)
+                                if s in self.outputs:
+                                    self.outputs.remove(s)
                                 inputs.remove(s)
                                 s.close()
 
-                                del message_queues[s]
+                                del self.message_queues[s]
                     for s in writable:
                         try:
-                            next_msg = message_queues[s].get_nowait()
+                            next_msg = self.message_queues[s].get_nowait()
                         except queue.Empty:
                             print(' ', s.getpeername(), 'queue empty()')
-                            outputs.remove(s)  # 该套接子没有要发送的内容，关闭套接子
+                            self.outputs.remove(s)  # 该套接子没有要发送的内容，关闭套接子
                         else:
                             print(' sending {!r} to {}'.format(next_msg, s.getpeername()))
                             s.send(next_msg)
@@ -140,37 +144,60 @@ class Network(QThread):
                     for s in exceptional:
                         print('exception conditon on', s.getpeername())
                         inputs.remove(s)
-                        if s in outputs:
-                            outputs.remove(s)
+                        if s in self.outputs:
+                            self.outputs.remove(s)
                         s.close()
 
-                        del message_queues[s]
+                        del self.message_queues[s]
 
         except Exception as e:
             self.robotCommunicationStatusSignal.emit('Break')
-            print('client is break!')
+            print('client is break!', e.args[0])
 
 
     def send(self, ctl, data=None):
         try:
             msg_to_send = self.msgManager.pack(ctl, data)
-            self.conn.sendall(msg_to_send)
-            print('[Info] Sent:', ctl, )
+            self.outputs.append(self.connectSocket)
+            self.message_queues[self.connectSocket].put(msg_to_send)
+            print('[Info] Sent:', ctl, data)
         except Exception as e:
             print(e)
 
 
-    def recv(self):
-        try:
-            data = self.conn.recv(40)
-        except BlockingIOError as e:
-            data = None
-        return self.msgManager.parse(data)
 
 
+from PyQt5.QtWidgets import QWidget, QTextEdit, QPushButton, QVBoxLayout
+class Client(QWidget):
+    def __init__(self, ip='localhost', port=6667):
+        super(Client, self).__init__()
+        self.net = Network()
+        self.net.start()
+        self.initUI()
 
+    def initUI(self):
+        self.sendTextbox = QTextEdit()
+        self.sendTextbox.setText('12, 3.14, 23.4, 23, 56.7, 34.4, 7673.3, 456.3, 121.1, 23.2')
+        self.btn = QPushButton('send')
+        self.btn.clicked.connect(self.slot_btn)
+        layout = QVBoxLayout()
+        layout.addWidget(self.sendTextbox)
+        layout.addWidget(self.btn)
+        self.setLayout(layout)
+        self.show()
+
+    def slot_btn(self):
+        txt = self.sendTextbox.toPlainText().split(',')
+        if len(txt) < 10:
+            print('[Error] Wrong typing.')
+        elif len(txt) == 10:
+            ctlBit = int(txt[0])
+            data = [float(x) for x in txt[1:]]
+            self.net.send(ctlBit, data)
+
+
+from time import sleep
 if __name__ == '__main__':
     app = QApplication(sys.argv)
-    net = Network()
-    net.start()
+    c = Client()
     sys.exit(app.exec())
